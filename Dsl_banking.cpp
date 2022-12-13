298964d6fcd3d96d2d52c07062880cee6048b544
Pankaj Ganage <91779900+pankajs2272003@users.noreply.github.com>
2022-12-13 10:43:22 +0530

    Create Dsl_banking.cpp

---
+++ Dsl_banking.cpp
@@ -0,0 +1,8 @@
+/*  Write a Python program that computes the net amount of a bank account based a 
+transaction log from console input. The transaction log format is shown as 
+following: D 100 W 200 (Withdrawal is not allowed if balance is going negative. 
+Write functions for withdraw and deposit) D means deposit while W means 
+withdrawal.
+Suppose the following input is supplied to the program: D 300, D 300 , W 200, D 100 Then, the
+output should be: 500  */
+
